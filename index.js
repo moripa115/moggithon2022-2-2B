5072522523cd062480cc1569705731766bcdce0c
Sohtatsu Ido <sohtatsu.ido@keio.jp>
2022-02-06 11:26:52 +0900

    restartのjs

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 const sounds = document.querySelectorAll(".weather_select_container button");
 const song = document.querySelector(".song");
 const video = document.querySelector(".video_container video");
-const play = document.querySelector(".play")
+const play = document.querySelector(".play");
+const replay = document.querySelector(".replay");
 
 sounds.forEach(sound => {
   sound.addEventListener("click", function() {
@@ -17,7 +18,16 @@ play.addEventListener("click", function() {
     checkPlaying(song);
 })
 
+replay.addEventListener("click", function(){
+    restartSong(song);
+})
 
+
+const restartSong = song => {
+  let currentTime = song.currentTime;
+  currentTime = 0;
+  console.log("restart!")
+}
 
 const checkPlaying = song => {
   if(song.paused) {
